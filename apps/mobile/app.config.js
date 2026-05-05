376d33dfe40fd4480c2dc9d248c72231ac0c40f6
Samet Yucel <sametyucel00@icloud.com>
2026-05-05 07:31:38 +0300

    Bump mobile store build numbers

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -46,7 +46,7 @@ module.exports = {
     ...(expo.ios ?? {}),
     supportsTablet: true,
     bundleIdentifier: "com.narrehberi.app",
-    buildNumber: "1.8",
+    buildNumber: "1.9",
     infoPlist: {
       ...(expo.ios?.infoPlist ?? {}),
       NSLocationWhenInUseUsageDescription:
@@ -56,7 +56,7 @@ module.exports = {
   android: {
     ...(expo.android ?? {}),
     package: "narrehberi.com",
-    versionCode: 9108
+    versionCode: 9109
   },
   extra: {
     ...(expo.extra ?? {}),
